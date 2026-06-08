6ab514a8e7c700ce1e7ddc12e2117a3b4fc76bf4
Jaguaro-ger <famigliacofani@gmail.com>
2026-06-08 14:56:46 +0200

    Add files via upload

--- giocatori.js
+++ giocatori.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Database dei dati dei giocatori perfettamente configurato
+    // Database dei dati dei giocatori resettato a 0
     const databaseGiocatori = {
-        raparelli: { name: "Matteo Raparelli", role: "Portiere #55", img: "rapa.JPG", stats: [{val: "50", label: "Minuti Giocati"}, {val: "4", label: "Gol Subiti"}, {val: "0", label: "MVP"}] },
-        muraru: { name: "Cristian Muraru", role: "Difensore #77", img: "ciccio.JPG", stats: [{val: "43", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
-        cofani: { name: "Davide Cofani", role: "Difensore #19", img: "jag.JPG", stats: [{val: "43", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
-        mastromattei: { name: "Daniele Mastromattei", role: "Difensore #22", img: "mastro.JPG", stats: [{val: "43", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
-        digiacomo: { name: "Valerio Di Giacomo", role: "Difensore #11", img: "diggia.JPG", stats: [{val: "43", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
-        marone: { name: "Christian Marone", role: "Centrocampista #10", img: "wallu.JPG", stats: [{val: "43", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
-        cava: { name: "Lorenzo Cava", role: "Centrocampista #7", img: "dude.JPG", stats: [{val: "43", label: "Minuti Giocati"}, {val: "1", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
-        potente: { name: "Lorenzo Potente", role: "Centrocampista #4", img: "potix.JPG", stats: [{val: "43", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
-        franci: { name: "Cristian Franci", role: "Centrocampista #4", img: "bryan.JPG", stats: [{val: "43", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
-        giovannetti: { name: "Emanuele Giovannetti", role: "Attaccante #24", img: "giova.JPG", stats: [{val: "50", label: "Minuti Giocati"}, {val: "4", label: "Goal Fatti"}, {val: "1", label: "MVP"}] },
-        ruotolo: { name: "Danilo Ruotolo", role: "Attaccante #21", img: "chinese.JPG", stats: [{val: "43", label: "Minuti Giocati"}, {val: "1", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
-        loturco: { name: "Andrea Lo Turco", role: "Attaccante #23", img: "lo turco.JPG", stats: [{val: "43", label: "Minuti Giocati"}, {val: "3", label: "Goal Fatti"}, {val: "0", label: "MVP"}] }
+        raparelli: { name: "Matteo Raparelli", role: "Portiere #55", img: "rapa.JPG", stats: [{val: "0", label: "Minuti Giocati"}, {val: "0", label: "Gol Subiti"}, {val: "0", label: "MVP"}] },
+        muraru: { name: "Cristian Muraru", role: "Difensore #77", img: "ciccio.JPG", stats: [{val: "0", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
+        cofani: { name: "Davide Cofani", role: "Difensore #19", img: "jag.JPG", stats: [{val: "0", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
+        mastromattei: { name: "Daniele Mastromattei", role: "Difensore #22", img: "mastro.JPG", stats: [{val: "0", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
+        digiacomo: { name: "Valerio Di Giacomo", role: "Difensore #11", img: "diggia.JPG", stats: [{val: "0", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
+        marone: { name: "Christian Marone", role: "Centrocampista #10", img: "wallu.JPG", stats: [{val: "0", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
+        cava: { name: "Lorenzo Cava", role: "Centrocampista #7", img: "dude.JPG", stats: [{val: "0", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
+        potente: { name: "Lorenzo Potente", role: "Centrocampista #4", img: "potix.JPG", stats: [{val: "0", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
+        franci: { name: "Cristian Franci", role: "Centrocampista #4", img: "bryan.JPG", stats: [{val: "0", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
+        giovannetti: { name: "Emanuele Giovannetti", role: "Attaccante #24", img: "giova.JPG", stats: [{val: "0", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
+        ruotolo: { name: "Danilo Ruotolo", role: "Attaccante #21", img: "chinese.JPG", stats: [{val: "0", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] },
+        loturco: { name: "Andrea Lo Turco", role: "Attaccante #23", img: "lo turco.JPG", stats: [{val: "0", label: "Minuti Giocati"}, {val: "0", label: "Goal Fatti"}, {val: "0", label: "MVP"}] }
     };
 
     // Estrae l'ID dall'URL della pagina (?id=nome)
